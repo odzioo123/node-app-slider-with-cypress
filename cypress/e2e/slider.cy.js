2c3e1eec81569c1abf11859e36d412142303ff6c
odzioo123 <134227251+odzioo123@users.noreply.github.com>
2025-03-20 10:31:18 +0100

    new test

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,20 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if second and third slide contains correct text. And Check if second slide contains correct image, and first slide contains correct image and do not contain second slides image', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.get('.card-image').should('be.visible');
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.card-image').should('not.be.visible');
+    cy.get('.card-contents').should('be.visible');
+  });
+});
